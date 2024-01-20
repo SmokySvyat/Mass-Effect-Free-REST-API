f011b8d8fdf722e070509695ea4b9e273df9fe54
SvyatLinux <swcnova@gmail.com>
2024-01-20 19:26:55 +0300

    fix: добавлена ссылка на проект в GitHub

--- frontend/me-api-frontend/src/components/Footer/Footer.jsx
+++ frontend/me-api-frontend/src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import './Footer.css';
 import telegram from '../../images/telegram-desktop-svgrepo-com.svg';
+import github from '../../images/github-svgrepo-com.svg';
 
 function Footer () {
     return (
@@ -10,6 +11,13 @@ function Footer () {
                 <a className='footer__link' href='https://t.me/mass_effect_api' target='blanc'>
                   <img className='footer__link-ico' src={telegram} alt='telegram logo'></img>
                   С вопросами, пожеланиями и предложениями писать в группу Telegram
+                </a>
+              </li>
+            
+              <li>
+                <a className='footer__link' href='https://github.com/SmokySvyat/Mass-Effect-Free-REST-API/' target='blanc'>
+                  <img className='footer__link-ico' src={github} alt='github logo'></img>
+                  Проект на GitHub
                 </a>
               </li>
             </ul>
