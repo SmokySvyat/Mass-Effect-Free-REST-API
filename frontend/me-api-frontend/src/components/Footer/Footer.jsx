a394c443c4175c1aa8013eef122486ae330e9432
SvyatLinux <swcnova@gmail.com>
2024-01-22 21:44:50 +0300

    feature: компонент footer адаптирован для мобильных разрешений

--- frontend/me-api-frontend/src/components/Footer/Footer.jsx
+++ frontend/me-api-frontend/src/components/Footer/Footer.jsx
@@ -1,28 +1,72 @@
 import './Footer.css';
+import { useEffect, useState } from 'react';
+
 import telegram from '../../images/telegram-desktop-svgrepo-com.svg';
 import github from '../../images/github-svgrepo-com.svg';
 
+import { MOBILE_RESOLUTION } from '../../utils/constants';
+
 function Footer () {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= MOBILE_RESOLUTION);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= MOBILE_RESOLUTION);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const footerContent = () => {
+    if (isMobile) {
+      return (
+        <div className='footer__content'>
+        <ul id='contacts' className='footer__contacts'>
+          <li className='footer__link-item'>
+            <a className='footer__link' href='https://t.me/mass_effect_api' target='blanc'>
+              <img className='footer__link-ico' src={telegram} alt='telegram logo'></img>
+            </a>
+          </li>
+
+          <li className='footer__link-item'>
+            <a className='footer__link' href='https://t.me/mass_effect_api' target='blanc'>
+              <img className='footer__link-ico' src={github} alt='github logo'></img>
+            </a>
+          </li>
+        </ul>
+        <p className='footer__copyrights'>С вопросами, пожеланиями и предложениями писать в группу Telegram</p>
+        <p className='footer__copyrights'> Святослав Нестеров</p>
+        </div>
+      )
+    } else {
+      return (
+        <div className='footer__content'>
+          <ul id='contacts' className='footer__contacts'>
+            <li className='footer__link-item'>
+              <img className='footer__link-ico' src={telegram} alt='telegram logo'></img>
+              <a className='footer__link' href='https://t.me/mass_effect_api' target='blanc'>
+                С вопросами, пожеланиями и предложениями писать в группу Telegram
+              </a>
+            </li>
+        
+            <li className='footer__link-item'>
+              <img className='footer__link-ico' src={github} alt='github logo'></img>
+              <a className='footer__link' href='https://github.com/SmokySvyat/Mass-Effect-Free-REST-API/' target='blanc'>
+                Проект на GitHub
+              </a>
+            </li>
+          </ul>
+          <p className='footer__copyrights'> Святослав Нестеров</p>
+        </div>
+      )
+    }
+  }
     return (
         <footer className='footer'>
-          <div className='footer__content'>
-            <ul id='contacts' className='footer__contacts'>
-              <li>
-                <a className='footer__link' href='https://t.me/mass_effect_api' target='blanc'>
-                  <img className='footer__link-ico' src={telegram} alt='telegram logo'></img>
-                  С вопросами, пожеланиями и предложениями писать в группу Telegram
-                </a>
-              </li>
-            
-              <li>
-                <a className='footer__link' href='https://github.com/SmokySvyat/Mass-Effect-Free-REST-API/' target='blanc'>
-                  <img className='footer__link-ico' src={github} alt='github logo'></img>
-                  Проект на GitHub
-                </a>
-              </li>
-            </ul>
-            <p className='footer__copyrights'> Святослав Нестеров</p>
-          </div>
+          {footerContent()}
         </footer>
     )
 }
--- frontend/me-api-frontend/src/utils/constants.js
+++ frontend/me-api-frontend/src/utils/constants.js
@@ -1,7 +1,9 @@
 const BASE_MEAPI_URL_RU = 'meapi.ru/api/';
 const BASE_MEAPI_URL_ON = 'meapi.online/api/';
+const MOBILE_RESOLUTION = 856;
 
 module.exports = {
     BASE_MEAPI_URL_RU,
     BASE_MEAPI_URL_ON,
+    MOBILE_RESOLUTION,
 };
