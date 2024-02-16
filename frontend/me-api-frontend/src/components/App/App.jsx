dd46143655a2c2c52fe147db639ad2c0d844c62c
SvyatLinux <swcnova@gmail.com>
2024-02-16 12:30:11 +0300

    fix: изменено расширение js файла компонента app на jsx

---
+++ frontend/me-api-frontend/src/components/App/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+import Header from '../Header/Header';
+import Navigation from '../Navigation/Navigation';
+import Footer from '../Footer/Footer';
+import Main from '../Main/Main';
+
+function App() {
+
+  return (
+    <div className="app">
+      <Header />
+      <Navigation />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
