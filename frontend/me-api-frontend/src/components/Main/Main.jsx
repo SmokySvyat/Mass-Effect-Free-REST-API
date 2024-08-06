743066bba8b83963d4470c6f8a8cf1a2ee29dc82
SvyatLinux <swcnova@gmail.com>
2024-08-06 10:16:12 +0300

    refactor: part.1 рефактор кода главной страницы, наполнение языквого файла

--- frontend/me-api-frontend/src/components/Main/Main.jsx
+++ frontend/me-api-frontend/src/components/Main/Main.jsx
@@ -2,175 +2,86 @@ import './Main.css';
 import TryApp from '../TryApp/TryApp';
 
 function Main ({language}) {
+    function Description({description}) {
+        const {
+            title,
+            paragraphs,
+            subtitle,
+            paragraphsSt
+        } = description;
+        
+        return (
+            <>
+            <h2>{title}</h2>
+            {paragraphs.map((el, i) => {
+                return (
+                    <p key={i++}>{el}</p>
+                )
+            })}
+            <h3>{subtitle}</h3>
+            {paragraphsSt.map((el, i) => {
+                return (
+                    <p key={i++}>{el}</p>
+                )
+            })}
+            </>
+        )
+    }
+
+    function Specification({specification}) {
+        console.log(specification)
+
+        const content = () => {
+            return (
+            specification.map((article, i) => {
+                const {
+                    heading,
+                    endpoint,
+                    paragraphs,
+                    code,
+                    errors
+                } = article;
+                console.log(code)
+                return (
+                    <li className='spec-list__item' key={i++}>
+                        <h4 className='spec-list__item-title'>{heading}{endpoint ? <span className='span-url'>{` | ${endpoint}`}</span> : ''}</h4>
+                        {paragraphs.map((el, i) => {
+                            return (
+                                <p key={i++} className='spec-list__item-description'>{el}</p>
+                            )
+                        })}
+                        {code ? <pre key={i++} className='code' dangerouslySetInnerHTML={code} /> : ''}
+                        {errors ? <p className='spec-list__item-description'>{errors.text} <a href={errors.link} className='span-error'>{errors.title}</a></p> : ''}
+                    </li>
+                )
+            }))
+        }
+
+        return (
+            <>
+                <article>
+                    <h3>Запросы и эндпоинты</h3>
+                    <ul className='spec-list'>
+                        {content()}
+                    </ul>
+                </article>
+            </>
+        )
+    }
     return (
         <main className="main">
-        <article  id="about" className='article'>
-          <h2>Описание</h2>
-          <p>
-            Mass Effect API это програмно доступная база данных трилогии Mass Effect с информацией
-            о ключевых персонажах, расах, планетах, для тех кто не знаком с франшизой, даже с информацией
-            о каждой игре трилогии.
-          </p>
-          <p>
-            Подойдёт как для обучения работы в программах Postman и ему подобных,
-            так и для тестирования свего frontend интерфейса.
-          </p>
-          <h3>Попробуй</h3>
-          <TryApp
-            language = {language}
-          />
-          
-        </article>
-        <article id="spec" className="article">
-          <h2>Спецификация</h2>
-          <ul className='spec-list'>
-            <h3 className='spec-list__title'>Запросы и эндпоины</h3>
-            <p className='spec-list__description'>
-                Базовым URL является <span className='span-url'>meapi.ru/api/</span> или <span className='span-url'>meapi.online/api/</span> реализованные
-                 типы запросов и эндпоины прописаны ниже.</p>
-            <li className='spec-list__item'>
-                <h4 className='spec-list__item-title'>GET | <span  className='span-url'>collection-name/page/0-0</span></h4>
-                <p className='spec-list__item-description'>
-                    Возвращает массив всех объектов из коллекции.
-                    <br />Доступные коллекции: <span className='span-collection'>planets</span>, <span className='span-collection'>races</span>, <span className='span-collection'>characters</span>. 
-                    <br />Пример объекта из массива коллекции <span className='span-collection'>planets</span> по 
-                    эндпоинту <span  className='span-url'>/planets/page/0-0</span>:
-                </p>
-                <pre className='code' dangerouslySetInnerHTML={
-                    {__html: `
-{
-    "_id": "6548db28c383a41ab1b432ab",
-    "image": "",
-    "systemName": "",
-    "links": "",
-    "episode": "Mass Effect 2",
-    "type": {
-        "planet": "true",
-        "satelite": "false",
-        "colony": "true",
-        "station": "false"
-    },
-    "codex": "",
-    "nameEN": "Amaterasu",
-    "descriptionEN": "Amaterasu is a human colony, home to Ashley Williams' mother and sisters. It lies about a dozen light-years from the Czarnobóg Fleet Depot and is one of the smaller Alliance colonies. In 2181, Ashley took a week of extraordinary leave to return home to take care of her youngest sister, Sarah. Amaterasu is a terminus on the Illium-Amaterasu shipping lane. In 2185, as security advisories are being issued against travel to human colonies, none are issued for Amaterasu, possibly due to its proximity to an Alliance base.",
-    "nameRU": "Аматерасу",
-    "descriptionRU": "Аматерасу (англ. Amaterasu — колония людей, дом Эшли Уильямс, её матери и сестры. Эта колония лежит в дюжине световых лет от Быстроходного Склада Кзарнобог, и является одной из малых колоний Альянса. В 2181 году, Эшли взяла неделю экстраординарного отпуска, чтобы вернуться домой, и позаботиться о своей младшей сестре, Саре. Аматерасу — конечная остановка на маршруте Иллиум-Аматерасу (узнать можно, посмотрев расписание отправлений межпланетных шаттлов на стоянке такси Нос-Астра). Также там можно увидеть отменённые рейсы и оповещения безопасности, которые предостерегают инопланетян от посещения человеческих колоний. Однако, ни одно оповещение не было выпущено для Аматерасу, возможно из-за близости этой колонии к границе с пространством Цитадели. На планету регулярно отправляется шаттл из космопорта Нос-Астра, Иллиум."
-}
-`}} 
+            <section  id="about" className='section'>
+                <Description 
+                    description = {language.description}
                 />
-            </li>
-
-            <li className='spec-list__item'>
-                <h4 className='spec-list__item-title'>GET | <span  className='span-url'>collection-name/object/object-id</span></h4>
-                <p className='spec-list__item-description'>
-                    Возвращает объект с запрашиваемым ID Например <span className='span-url'>/api/planets/object/6548db28c383a41ab1b432ab</span> вернёт 
-                    объект из примера выше
-                </p>
-            </li>
-
-            <li className='spec-list__item'>
-                <h4 className='spec-list__item-title'>GET | <span className='span-url'>collection-name/page/:page-:range</span></h4>
-                <p className='spec-list__item-description'>
-                    Возвращет массив объектов постранично. С указанием: общего количества документов в массиве, количеством документов на странице, 
-                    номер текущей страницы, общим количеством страниц. Например <span className='span-url'>/api/planets/page/1-10</span> вернет первую 
-                    страницу коллекции <span className='span-collection'>planets</span> с 10 объектами, <span className='span-url'>.../2-10</span> вторую 
-                    страницу, <span className='span-url'>.../3-10</span> третью и etc.
-                </p>
-                <pre className='code' dangerouslySetInnerHTML={
-                    {__html: `
-{
-    "total": 73,
-    "total_pages": 8,
-    "per_page": 10,
-    "page_now": 1,
-    "data": [
-        {
-            "_id": "6548db28c383a41ab1b432ab",
-            "image": "",
-            "systemName": "",
-            "links": "",
-            "episode": "Mass Effect 2",
-            "type": {
-                "planet": "true",
-                "satelite": "false",
-                "colony": "true",
-                "station": "false"
-            },
-            "codex": "",
-            "nameEN": "Amaterasu",
-            "descriptionEN": "Amaterasu is a human colony, home to Ashley Williams' mother and sisters. It lies about a dozen light-years from the Czarnobóg Fleet Depot and is one of the smaller Alliance colonies. In 2181, Ashley took a week of extraordinary leave to return home to take care of her youngest sister, Sarah. Amaterasu is a terminus on the Illium-Amaterasu shipping lane. In 2185, as security advisories are being issued against travel to human colonies, none are issued for Amaterasu, possibly due to its proximity to an Alliance base.",
-            "nameRU": "Аматерасу",
-            "descriptionRU": "Аматерасу (англ. Amaterasu — колония людей, дом Эшли Уильямс, её матери и сестры. Эта колония лежит в дюжине световых лет от Быстроходного Склада Кзарнобог, и является одной из малых колоний Альянса. В 2181 году, Эшли взяла неделю экстраординарного отпуска, чтобы вернуться домой, и позаботиться о своей младшей сестре, Саре. Аматерасу — конечная остановка на маршруте Иллиум-Аматерасу (узнать можно, посмотрев расписание отправлений межпланетных шаттлов на стоянке такси Нос-Астра). Также там можно увидеть отменённые рейсы и оповещения безопасности, которые предостерегают инопланетян от посещения человеческих колоний. Однако, ни одно оповещение не было выпущено для Аматерасу, возможно из-за близости этой колонии к границе с пространством Цитадели. На планету регулярно отправляется шаттл из космопорта Нос-Астра, Иллиум."
-        },
-]}
-                    `}
-                } />
-                <p className='spec-list__item-description'>
-                    Запрос несуществующей страницы вернет ошибку "<a href='#error-not-found' className='span-error'>ERROR_NOT_FOUND</a>".
-                </p>
-            </li>
-
-            <li className='spec-list__item'>
-                <h4 className='spec-list__item-title'>GET | <span className='span-url'>contacts/all</span></h4>
-                <p className='spec-list__item-description'>
-                    Возвращает массив объектов "безопасных" контактов (без значения пароля). 
-                    Пароль хэшируется и хранится в коллекции в зашифрованном виде, без возможности его просмотреть.
-                </p>
-            </li>
-
-            <li className='spec-list__item'>
-                <h4 className='spec-list__item-title'>GET | <span className='span-url'>contacts/:id</span></h4>
-                <p className='spec-list__item-description'>
-                    Возвращает объект "безопасного" контакта по запрашиваемому Id.
-                    Пример ответа на запрос по эндпоинту <span className='span-url'>/api/contacts/657d866188a46a437e241e6e</span>:
-                </p>
-                <pre className='code' dangerouslySetInnerHTML={
-                    {__html: `
-{
-    "id": "657d866188a46a437e241e6e",
-    "email": "shepard@meapi.gl",
-    "phone": "03334447788"
-}
-                    `}
-                } />
-                <p className='spec-list__item-description'>
-                    Запрос контакта по несуществующему Id возвращает объект с сообщением.
-                    <br />Ошибка "<a href='#not-found' className='span-error'>Not Found</a>".
-                </p>
-            </li>
-
-            <li className='spec-list__item'>
-                <h4 className='spec-list__item-title'>POST | <span className='span-url'>contacts/new</span></h4>
-                <p className='spec-list__item-description'>
-                    Создаёт документ в базе данных. В качестве ответа возвращает объект с email созданного контакта.
-                    Тело запроса должно быть в формате JSON следующей структуры:
-                </p>
-                <pre className='code' dangerouslySetInnerHTML={
-                    {__html: `
-{
-    "email": "email@example.com",
-    "password": "Password2!",
-    "phone": "03334447788"
-}
-                    `}
-                } />
-                <p className='spec-list__item-description'>
-                    Значение email валидируется на уникальность и на соотвествие шаблону email. Значение phone валидируется на уникальность, 
-                    минимальную дину в 10 цифр и на присутсвие в строке только чисел. Пароль валидируется на минимальную длину в 8 символов, 
-                    шифруется и хранится в таком состоянии в коллекции. При успешном создании документа к нему автоматически добавляется 
-                    ключ <span className='code'>createdAt:</span> со значением даты создания документа в формате <span className='code'>2023-12-18T07:19:25.511+00:00</span>.
-                </p>
-                <p className='spec-list__item-description'>
-                    Если какое-либо из значений не уникально, документ не создаётся и возвращается объект с сообщением об ошибке и указанием, что именно не уникально.
-                </p>
-                <p className='spec-list__item-description'>
-                    При передаче некорректных данных, когда значение email не соответсвует шаблону и/или в значении phone 
-                    содержится что-то кроме цифр, документ не создаётся, возвращается объект с сообщением об ошибке соответсвующего характера.
-                </p>
-                <p className='spec-list__item-description'>
-                    Ошибка "<a href='#not-unique' className='span-error'>Not Unique</a>".
-                </p>
-            </li>
+                <TryApp
+                    language = {language}
+                />
+            </section>
+        <section id="spec" className="section">
+            <h2>Спецификация</h2>
+          <ul className='spec-list'>
+            <Specification specification={language.specification} />
 
             <li className='spec-list__item'>
                 <h4 className='spec-list__item-title'>PATCH | <span className='span-url'>contacts/:id</span></h4>
@@ -228,20 +139,8 @@ function Main ({language}) {
                     <br />Ошибка "<a href='#not-found' className='span-error'>Not Found</a>".
                 </p>
             </li>
-
-            <li className='spec-list__item'>
-                <h4 className='spec-list__item-title'> | <span className='span-url'></span></h4>
-                <p className='spec-list__item-description'>
-                </p>
-                <pre className='code' dangerouslySetInnerHTML={
-                    {__html: `
-{
-}
-                    `}
-                } />
-            </li>
           </ul>
-        </article>
+        </section>
       </main>
     )
 }
